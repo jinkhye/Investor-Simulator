4decb90d92941e393fc2095de1c9739345ed722b
coolkheng <122100699+coolkheng@users.noreply.github.com>
2024-04-15 20:56:34 +0800

    Update News Assessment

--- investor_simulator/lib/dialog/news_dialog.dart
+++ investor_simulator/lib/dialog/news_dialog.dart
@@ -1,17 +1,12 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:get/get.dart';
 import 'package:investor_simulator/constant/color.dart';
 import 'package:investor_simulator/models/news_details_model.dart';
-import 'package:investor_simulator/models/news_model.dart';
-import 'package:investor_simulator/pages/news_assessment.dart';
 import 'package:investor_simulator/provider/news_provider.dart';
+import 'package:investor_simulator/pages/news_assessment.dart';
 import 'package:provider/provider.dart';
 import 'package:stroke_text/stroke_text.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
+import 'package:get/get.dart';
 
 void openNewsDialog(BuildContext context, String newsURL, String pubDate) {
   final provider = Provider.of<NewsProvider>(context, listen: false);
@@ -69,7 +64,6 @@ void openNewsDialog(BuildContext context, String newsURL, String pubDate) {
                         ),
                       ),
                     ),
-                    aiAssessment(context),
                   ],
                 ),
               ),
@@ -181,6 +175,7 @@ SingleChildScrollView newsPage(
           const SizedBox(
             height: 10,
           ),
+          aiAssessment(context, content),
         ],
       ),
     ),
@@ -215,12 +210,18 @@ Widget webBrowser(String url) {
   );
 }
 
-ElevatedButton aiAssessment(BuildContext context) {
+ElevatedButton aiAssessment(
+  BuildContext context,
+  NewsDetails content,
+) {
   return ElevatedButton(
     onPressed: () {
-      // Get.to(() => NewsAssessment(news: news, index: index),
-      //     transition: Transition.circularReveal,
-      //     duration: const Duration(milliseconds: 800));
+      Get.to(
+          () => NewsAssessment(
+                news: content,
+              ),
+          transition: Transition.circularReveal,
+          duration: const Duration(milliseconds: 800));
     },
     style: ElevatedButton.styleFrom(
       padding: const EdgeInsets.all(0),
--- investor_simulator/lib/pages/news_assessment.dart
+++ investor_simulator/lib/pages/news_assessment.dart
@@ -1,158 +1,249 @@
 import 'package:flutter/material.dart';
 import 'package:investor_simulator/constant/color.dart';
 import 'package:investor_simulator/menu/topMenu.dart';
+import 'package:investor_simulator/models/news_details_model.dart';
 import 'package:investor_simulator/models/news_model.dart';
 import 'package:stroke_text/stroke_text.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NewsAssessment extends StatelessWidget {
-  const NewsAssessment({super.key, required this.news, required this.index});
+  final NewsDetails news;
 
-  final List<NewsModel> news;
-  final int index;
+  const NewsAssessment({super.key, required this.news});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [purple, darkPurple], // Adjust colors as needed
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              topMenu(context),
-              const Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Scrollbar(
-                    thumbVisibility:
-                        true, // Ensure the scrollbar is always visible
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 20),
-                          Align(
-                            alignment: Alignment.center,
-                            child: StrokeText(
-                              text: "NEWS ASSESSMENT",
-                              textStyle: TextStyle(fontSize: 40, color: yellow),
-                              strokeColor: darkPurple,
-                              strokeWidth: 7,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          Divider(
-                            thickness: 4,
-                          ),
-                          Text(
-                            'news[index].name',
-                            style: TextStyle(
-                              letterSpacing: 0.8,
-                              fontSize: 30,
-                              color: white,
-                              overflow: TextOverflow.clip,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 10),
-                          // Center(
-                          //   child: Container(
-                          //     width: 150,
-                          //     height: 150,
-                          //     decoration: BoxDecoration(
-                          //       borderRadius: BorderRadius.circular(16),
-                          //       image: DecorationImage(
-                          //         image: AssetImage(news[index].iconPath),
-                          //         fit: BoxFit.fill,
-                          //       ),
-                          //       border: const Border.fromBorderSide(
-                          //         BorderSide(
-                          //           color: white,
-                          //           width: 4,
-                          //         ),
-                          //       ),
-                          //     ),
-                          //   ),
-                          // ),
-                          SizedBox(height: 10),
-                          Divider(
-                            thickness: 4,
-                          ),
-                          SizedBox(height: 20),
-                          Text(
-                            'Based on Tesla\'s advancement in battery technology, there is a strong likelihood that Tesla\'s stock price will rise.',
-                            style: TextStyle(
-                              fontFamily: 'Helvetica',
-                              fontSize: 16,
-                              color: white,
-                              fontWeight: FontWeight.w600,
-                              wordSpacing: 0,
-                              letterSpacing: 0,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 20),
-                          Text(
-                            '1. Market Confidence:\n',
-                            style: TextStyle(
-                              fontFamily: 'Helvetica',
-                              fontSize: 20,
-                              color: yellow,
-                              fontWeight: FontWeight.w800,
-                              wordSpacing: 0,
-                              letterSpacing: 0,
-                            ),
-                          ),
-                          Text(
-                            'Investors are likely to react positively to the news, as it demonstrates Tesla\'s continued leadership in innovation within the electric vehicle industry.\n',
-                            style: TextStyle(
-                              fontFamily: 'Helvetica',
-                              fontSize: 16,
-                              color: white,
-                              fontWeight: FontWeight.w600,
-                              wordSpacing: 0,
-                              letterSpacing: 0,
-                            ),
-                          ),
-                          Text(
-                            '2. Competitive Advantage:\n',
-                            style: TextStyle(
-                              fontFamily: 'Helvetica',
-                              fontSize: 20,
-                              color: yellow,
-                              fontWeight: FontWeight.w800,
-                              wordSpacing: 0,
-                              letterSpacing: 0,
-                            ),
-                          ),
-                          Text(
-                            'Tesla\'s next-generation battery cell with superior energy density and longevity gives the company a competitive edge over rivals. This innovation by Tesla is likely to attract investors who anticipate long-term growth.',
-                            style: TextStyle(
-                              fontFamily: 'Helvetica',
-                              fontSize: 16,
-                              color: white,
-                              fontWeight: FontWeight.w600,
-                              wordSpacing: 0,
-                              letterSpacing: 0,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          SizedBox(height: 40),
-                        ],
-                      ),
-                    ),
-                  ),
+    return FutureBuilder<List<String>>(
+      future: fetchText(news),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text('Error: ${snapshot.error}'),
+            ),
+          );
+        } else {
+          List<String> textList = snapshot.data ?? [];
+          return Scaffold(
+            body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [purple, darkPurple],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    topMenu(context),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Scrollbar(
+                          thumbVisibility: true,
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(height: 20),
+                                const Align(
+                                  alignment: Alignment.center,
+                                  child: StrokeText(
+                                    text: "NEWS ASSESSMENT",
+                                    textStyle:
+                                        TextStyle(fontSize: 40, color: yellow),
+                                    strokeColor: darkPurple,
+                                    strokeWidth: 7,
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                const Divider(
+                                  thickness: 4,
+                                ),
+                                Text(
+                                  news.title,
+                                  style: const TextStyle(
+                                    letterSpacing: 0.8,
+                                    fontSize: 30,
+                                    color: white,
+                                    overflow: TextOverflow.clip,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 10),
+                                Center(
+                                  child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(16),
+                                      image: DecorationImage(
+                                        image: NetworkImage(news.topImage),
+                                        fit: BoxFit.cover,
+                                      ),
+                                      border: const Border.fromBorderSide(
+                                          BorderSide(
+                                        color: black,
+                                        width: 4,
+                                      )),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                const Divider(
+                                  thickness: 4,
+                                ),
+                                const SizedBox(height: 20),
+                                Text(
+                                  textList[0],
+                                  style: const TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 16,
+                                    color: white,
+                                    fontWeight: FontWeight.w600,
+                                    wordSpacing: 0,
+                                    letterSpacing: 0,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 20),
+                                const Text(
+                                  '1. Market Confidence:\n',
+                                  style: TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 20,
+                                    color: yellow,
+                                    fontWeight: FontWeight.w800,
+                                    wordSpacing: 0,
+                                    letterSpacing: 0,
+                                  ),
+                                ),
+                                Text(
+                                  textList[1],
+                                  style: const TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 16,
+                                    color: white,
+                                    fontWeight: FontWeight.w600,
+                                    wordSpacing: 0,
+                                    letterSpacing: 0,
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                const Text(
+                                  '2. Company performance:\n',
+                                  style: TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 20,
+                                    color: yellow,
+                                    fontWeight: FontWeight.w800,
+                                    wordSpacing: 0,
+                                    letterSpacing: 0,
+                                  ),
+                                ),
+                                Text(
+                                  textList[2],
+                                  style: const TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 16,
+                                    color: white,
+                                    fontWeight: FontWeight.w600,
+                                    wordSpacing: 0,
+                                    letterSpacing: 0,
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                const Text(
+                                  '3. Investment Recommendation:\n',
+                                  style: TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 20,
+                                    color: yellow,
+                                    fontWeight: FontWeight.w800,
+                                    wordSpacing: 0,
+                                    letterSpacing: 0,
+                                  ),
+                                ),
+                                Text(
+                                  textList[3],
+                                  style: const TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 16,
+                                    color: white,
+                                    fontWeight: FontWeight.w600,
+                                    wordSpacing: 0,
+                                    letterSpacing: 0,
+                                  ),
+                                ),
+                                const SizedBox(height: 40),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+      },
     );
+  }
+
+  Future<List<String>> fetchText(NewsDetails newss) async {
+    final response = await http.post(
+      Uri.parse('https://api.chatanywhere.cn/v1/chat/completions'),
+      headers: {
+        'Authorization':
+            'Bearer sk-3AfXb4qpHinspY3jGEuCarfvXRp82smqBIYCDqrpJ0JBYOd9',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'model': 'gpt-3.5-turbo-0125',
+        'messages': [
+          {
+            'role': "user",
+            'content':
+                "You are now an AI stock's news analyzer . I strictly require you to analyze the news and give your opinion on the news. "
+          },
+          {
+            'role': "user",
+            'content':
+                "These are the few main question that you have to answer : Q1. How will the news impacting the specific stocks ? (the stock is more likely to rise or drop?) Q2. How is the market confidence? (how would the investors react to the news?) Q3. How is company performance ? Q4. what is your investment recommendations ?(buy , hold , sell)"
+          },
+          {
+            'role': "user",
+            'content':
+                "News: In a groundbreaking announcement today, Tesla revealed a revolutionary advancement in battery technology that promises to reshape the electric vehicle industry and significantly accelerate the global transition to sustainable energy. The unveiling of this cutting-edge innovation sent shockwaves through the financial markets, propelling Tesla's stock price to new heights.CEO Elon Musk introduced the world a next-generation battery cell with unparalleled energy density and longevity. This leap forward in battery technology is aimed to address the longstanding challenges of range anxiety and charging times, effectively eliminating barriers to widespread EV adoption."
+          },
+          {
+            'role': "user",
+            'content':
+                "Answer each questions in sentence with brief and informative. I want only the answer in your response without labelling the question number. "
+          },
+        ],
+        'max_tokens': 1700,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      String content = data['choices'][0]['message']['content'];
+      List<String> messages = content.split(
+          '. '); // Split the response into an array based on newline characters
+      return messages;
+    } else {
+      throw Exception('Failed to load text:${response.body}');
+    }
   }
 }
